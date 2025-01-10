c13b3fc491ed45eae6c3168e022f1310a3ba73de
Alberto Carmona Sicre <albcarsic@alum.us.es>
2025-01-10 17:53:09 +0100

    Pruebas

--- app/modules/auth/tests/test_unit.py
+++ app/modules/auth/tests/test_unit.py
@@ -117,3 +117,19 @@ def test_service_create_with_profile_fail_no_password(clean_database):
 
     assert UserRepository().count() == 0
     assert UserProfileRepository().count() == 0
+
+def test_conteo_exitoso(clean_database):
+    response = test_client.post(
+        "/login", data=dict(email="test@example.com", password="test1234"), follow_redirects=True
+    )
+
+    assert response.request.path != url_for("auth.login"), "Login was unsuccessful"
+
+    test_client.get("/logout", follow_redirects=True)
+
+def test_login_falla_con_campos_vacíos(test_client):
+    response = test_client.post(
+        "/login", data=dict(email="", password=""), follow_redirects=True
+    )
+
+    assert response.request.path == url_for("auth.login"), "Login was unsuccessful"
